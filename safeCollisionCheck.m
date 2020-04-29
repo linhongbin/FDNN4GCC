ac6e81e4634e7cc3bf5552a6fa3581b1b983c18c
Ben Lin <522181941@qq.com>
2020-04-29 18:34:02 +0800

    safe checking

---
+++ safeCollisionCheck.m
@@ -0,0 +1,13 @@
+ARM_NAME = 'MTMR'
+SN = '12345'
+load_file = fullfile('data', [ARM_NAME, '_',SN], 'real', 'uniform', 'raw_data', 'desired_pivot_points.mat')
+load(load_file)
+mtm_arm = mtm(ARM_NAME)
+
+config_mat_safeCheck = config_mat_safeCheck(:, [2:2:end]);
+for i = 1:size(config_mat_safeCheck, 2)
+    pos = [config_mat_safeCheck(:,i).', 0];
+    mtm_arm.move_joint(deg2rad(pos));
+
+    disp(sprintf("finish: (%d / %d)", i, size(config_mat_safeCheck, 2)))
+end
